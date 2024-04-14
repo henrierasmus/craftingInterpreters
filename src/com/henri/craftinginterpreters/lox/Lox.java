63fcc4e91378d9a3b909da85911d07f17f760a84
henri <henri.erasmus@hotmail.com>
2024-04-14 16:07:35 +0200

    Implemented Interpreter

--- src/com/henri/craftinginterpreters/lox/Lox.java
+++ src/com/henri/craftinginterpreters/lox/Lox.java
@@ -10,6 +10,8 @@ import java.util.List;
 
 public class Lox {
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
+    private static final Interpreter interpreter = new Interpreter();
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -27,6 +29,8 @@ public class Lox {
 
         if (hadError)
             System.exit(65);
+        if (hadRuntimeError)
+            System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -52,7 +56,7 @@ public class Lox {
         if (hadError)
             return;
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -72,5 +76,12 @@ public class Lox {
             report(token.line, " at end", message);
         else
             report(token.line, " at '" + token.lexeme + "'", message);
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+                "\n[line" + error.token.line + "]");
+
+        hadRuntimeError = true;
     }
 }
